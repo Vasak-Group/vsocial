0fd71839a00919732c3ad93b119c0f978db35afe
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-14 19:20:45 -0300

    Add Font Awesome icons and components

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faRightToBracket,
+  faRightFromBracket,
+  faUserPlus,
+  faGear,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
+
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+library.add(faRightToBracket, faRightFromBracket, faUserPlus, faGear, faHouse);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 app.mount("#app");
