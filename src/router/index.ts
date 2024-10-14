a0c9db1479291200eebd718f59a15daf92ba8de8
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-14 07:35:09 -0300

    Add routes for HomeView, LoginView, and RegisterView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/auth/LoginView.vue";
+import RegisterView from "../views/auth/RegisterView.vue";
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: HomeView,
+  },
+  {
+    path: "/login",
+    component: LoginView,
+  },
+  {
+    path: "/register",
+    component: RegisterView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
