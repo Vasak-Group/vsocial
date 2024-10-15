9c977b72a7f9fce5317307034c5757bfb9eba4ec
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-15 07:50:47 -0300

    Refactorizar estilos en FooterComponent.vue y HomeView.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,12 @@
-<script lang="ts"></script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="p-3">
     <!--dark mode - text and icons-->
     <footer
-      class="p-3 text-slate-700 dark:text-gray-400 bg-white dark:bg-gray-900 rounded-lg shadow-lg font-medium"
+      class="p-3 text-slate-700 dark:text-gray-400 bg-white dark:bg-gray-900 rounded-xl shadow-lg font-medium"
     >
-    <p class="text-center">VSocial powered by Vasak Group</p>
+    <p class="text-center common-text">VSocial powered by Vasak Group</p>
   </footer>
   </div>
 </template>
