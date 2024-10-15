d896b459c8ac9eddaa84e1413840892b9b70fbaa
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-15 06:51:53 -0300

    Add DarkModeButton component for managing dark mode state and UI

---
+++ src/components/buttons/DarkModeButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useDarkModeStore } from '@/stores/darkMode';
+import { onMounted } from 'vue';
+
+const darkModeStore = useDarkModeStore();
+
+onMounted(() => {
+  darkModeStore.setBodyClass();
+});
+</script>
+
+<template>
+  <span @click="darkModeStore.changeDarkMode()" class="hover:text-white cursor-pointer ml-1">
+    <font-awesome-icon
+      :icon="darkModeStore.getIcon"
+      class="p-3 bg-slate-200 dark:bg-gray-800 rounded-full ml-6"
+    />
+  </span>
+</template>
