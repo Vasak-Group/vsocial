a75b8538314e6ea28cf7d3061ccd2e88eae1d76e
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-15 06:49:56 -0300

    Add pinia config and icons

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
@@ -7,6 +8,8 @@ import {
   faUserPlus,
   faGear,
   faHouse,
+  faMoon,
+  faSun,
 } from "@fortawesome/free-solid-svg-icons";
 
 import "./style.css";
@@ -14,9 +17,19 @@ import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-library.add(faRightToBracket, faRightFromBracket, faUserPlus, faGear, faHouse);
+library.add(
+  faRightToBracket,
+  faRightFromBracket,
+  faUserPlus,
+  faGear,
+  faHouse,
+  faMoon,
+  faSun
+);
 
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
+app.use(pinia);
 app.mount("#app");
