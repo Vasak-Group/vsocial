821788ab1e41d1443de8b5dbf96fb0872382ff45
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-16 13:47:47 -0300

    Refactorizar servicio VSocialConnector para manejar errores en las solicitudes GET y POST

--- src/services/vsocial.connector.ts
+++ src/services/vsocial.connector.ts
@@ -1,30 +1,40 @@
 const VSocialConnector = {
   GET: async (url: string, token: string = "") => {
-    const response = await fetch(
-      import.meta.env.VITE_BACK_SERVICE.concat(url),
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return response.json();
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACK_SERVICE.concat(url),
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   },
   POST: async (url: string, data: any, token: string = "") => {
-    const response = await fetch(
-      import.meta.env.VITE_BACK_SERVICE.concat(url),
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(data),
-      }
-    );
-    return response.json();
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACK_SERVICE.concat(url),
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   },
 };
 
