998b3f5243388765b68918e8720a7a784df4f254
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-16 10:12:10 -0300

    Refactorizar estilos en NavBarComponent.vue

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -10,18 +10,19 @@ import DarkModeButton from "./buttons/DarkModeButton.vue";
       class="p-3 common-text bg-white dark:bg-gray-900 rounded-xl shadow-lg font-medium capitalize flex content-center items-center"
     >
       <span class="px-2 mr-2 border-xl border-gray-800"> VSocial </span>
-
-      <RouterLink
-        to="/"
-        class="px-2 py-1 mx-2 cursor-pointer hover:bg-slate-200 dark:hover:bg-gray-800 hover:text-white text-sm rounded-xl flex content-center items-center"
-      >
-        <font-awesome-icon
-          icon="fas fa-home"
-          class="p-3 bg-slate-200 dark:bg-gray-800 rounded-full w-4 h-4"
-        />
-        <span class="mx-1"> Home </span>
-      </RouterLink>
-      <div class="ml-auto">
+      <div class="ml-auto mr-auto">
+        <RouterLink
+          to="/"
+          class="px-2 py-1 mx-2 cursor-pointer hover:bg-slate-200 dark:hover:bg-gray-800 hover:text-white text-sm rounded-xl flex content-center items-center"
+        >
+          <font-awesome-icon
+            icon="fas fa-home"
+            class="p-3 bg-slate-200 dark:bg-gray-800 rounded-full w-4 h-4"
+          />
+          <span class="mx-1"> Home </span>
+        </RouterLink>
+      </div>
+      <div>
         <RouterLink to="/login" class="hover:text-white cursor-pointer ml-1">
           <font-awesome-icon
             icon="fas fa-right-to-bracket"
