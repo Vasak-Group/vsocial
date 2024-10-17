f8ebb51934526d474652c692959e2cdc2287f2e7
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-17 06:59:23 -0300

    Refactorizar rutas y agregar vistas de dashboard
    - Refactorizar importación de useUserSessionStore en NavBarComponent.vue
    - Refactorizar el manejo de errores en el formulario de inicio de sesión
    - Refactorizar el manejo de errores en el registro de usuario
    - Refactorizar el almacenamiento de la sesión del usuario
    - Agregar vista DashboardView
    - Agregar vista DashStatView en el directorio de dashboard
    - Actualizar archivo de rutas para incluir las nuevas vistas de dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/auth/LoginView.vue";
 import RegisterView from "@/views/auth/RegisterView.vue";
+import DashStatView from "@/views/dashboard/DashStatView.vue";
+import DashboardView from "@/views/DashboardView.vue";
+import HomeView from "@/views/HomeView.vue";
+
+const publicPages = ["/login", "/register", "/"];
+const invitePages = ["/login", "/register"];
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,11 +21,34 @@ const routes: Array<RouteRecordRaw> = [
     path: "/register",
     component: RegisterView,
   },
+  {
+    path: "/dashboard",
+    component: DashboardView,
+    children: [
+      {
+        path: "",
+        component: DashStatView,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const authRequired = !publicPages.includes(to.path);
+  const onlyInvite = invitePages.includes(to.path);
+  const loggedIn = localStorage.getItem("vUserSessionToken");
+
+  if (to.path !== "/login" && authRequired && !loggedIn) {
+    return next({ path: "/login" });
+  } else if (to.path !== "/dashboard" && onlyInvite && loggedIn) {
+    return next({ path: "/dashboard" });
+  }
+  next();
 });
 
 export default router;
