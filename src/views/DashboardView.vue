d0e93ee0f1a0751259f5e6e480e0a0b87be60b6c
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-17 10:23:02 -0300

    Refactorizar DashboardView.vue para agregar componente SideBarComponent.vue

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { RouterView } from "vue-router";
+import SideBarComponent from "@/components/SideBarComponent.vue";
 </script>
 
 <template>
-    DashboardView
+  <div class="flex">
+    <SideBarComponent />
     <RouterView />
+  </div>
 </template>
