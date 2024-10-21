85adabb73deabcd3539732d7d648362abf91b948
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-21 11:18:19 -0300

    Refactorizar componente BaseLayout.vue para agregar NavBarComponent y FooterComponent

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import FooterComponent from "@/components/FooterComponent.vue";
+import NavBarComponent from "@/components/NavBarComponent.vue";
+</script>
+
+<template>
+  <NavBarComponent />
+  <RouterView />
+  <FooterComponent />
+</template>
