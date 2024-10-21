87ef13a168446e20c24d8bc7fd7ec27db2e7832a
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-21 11:19:16 -0300

    Refactorizar el enrutador para agregar middleware de seguridad

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { secureMiddleware } from "@/middlewares/SecureURL.midedleware";
 import LoginView from "@/views/auth/LoginView.vue";
 import RegisterView from "@/views/auth/RegisterView.vue";
 import ConfigurationsView from "@/views/dashboard/ConfigurationsView.vue";
 import StatsView from "@/views/dashboard/StatsView.vue";
-import DashboardView from "@/views/DashboardView.vue";
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
 import HomeView from "@/views/HomeView.vue";
-
-const publicPages = ["/login", "/register", "/"];
-const invitePages = ["/login", "/register"];
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -24,7 +22,7 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: "/dashboard",
-    component: DashboardView,
+    component: DashboardLayout,
     children: [
       {
         path: "",
@@ -43,17 +41,7 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const authRequired = !publicPages.includes(to.path);
-  const onlyInvite = invitePages.includes(to.path);
-  const loggedIn = localStorage.getItem("vUserSessionToken");
+router.beforeEach(secureMiddleware);
 
-  if (to.path !== "/login" && authRequired && !loggedIn) {
-    return next({ path: "/login" });
-  } else if (to.path !== "/dashboard" && onlyInvite && loggedIn) {
-    return next({ path: "/dashboard" });
-  }
-  next();
-});
 
 export default router;
